042b1ff901160c7e0be2e305771cd98631cd4eea
Aaliyah <marial.aaliyahangelica@dnsc.edu.ph>
2026-05-29 01:35:04 +0800

    Connected Railway database

--- db.php
+++ db.php
@@ -1,12 +1,15 @@
 <?php
-$host = "sql300.infinityfree.com";
-$dbname = "if0_42032934_secureauth";
-$username = "if0_42032934";
-$password = "M2wDEkr4Te2";
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = mysqli_connect(
+    "mysql.railway.internal",
+    "root",
+    "hWOItqqXznmgvDJvKaKCbESBciRkxBvB",
+    "railway",
+    3306
+);
 
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
